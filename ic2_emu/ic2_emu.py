90a1d30dffa26f0f1e85546dc17762a22d7f76dc
Pip Jones <code@scipilot.org>
2017-04-19 08:48:10 +1000

    + console colour experiments - reduce debugging

--- ic2_emu/ic2_emu.py
+++ ic2_emu/ic2_emu.py
@@ -44,13 +44,13 @@ class Device(object):
 
     def writeList(self, register, data):
         """Write bytes to the specified register."""
-        self._logger.debug(".writeList:Writing list:%s to register 0x%02X", data, register)
+        # self._logger.debug(".writeList:Writing list:%s to register 0x%02X", data, register)
         self._bus.write_i2c_block_data(self._address, register, data)
 
     def write8(self, register, value):
         """Write an 8-bit value to the specified register."""
         value = value & 0xFF
-        self._logger.debug(".write8:Writing value:0x%02X to register 0x%02X", value, register)
+        # self._logger.debug(".write8:Writing value:0x%02X to register 0x%02X", value, register)
         self._bus.write_byte_data(self._address, register, value)
 
 
@@ -90,6 +90,8 @@ class textTableBus(object):
     """
     Writes all output into ASCII Table on the console
     Using https://github.com/foutaise/texttable/
+    
+    Stores the data internally in a two-colour per pixel bitmap: 00=off, 01=Green, 10=Red, 11=Yellow
     """
 
     def __init__(self, busnum):
@@ -99,10 +101,13 @@ class textTableBus(object):
 
     def draw(self):
         table = Texttable()
-        #table.set_cols_align(["l", "r", "c"])
+        # table.set_deco(Texttable.HEADER)
+        # table.set_cols_width([15 for j in range(8)])
+        # table.set_cols_align(['c' for j in range(8)])
+        # table.set_cols_dtype(['t' for j in range(8)])
         #table.set_cols_valign(["t", "m", "b"])
         # Column indexes
-        table.add_row(["1", "2", "3", "4", "5", "6", "7", "8"])
+        table.add_row([j for j in range(8)])
         for row in self.data:
             # todo prepend row index?
             table.add_row([self.render_pixel(col) for col in row])
@@ -113,26 +118,31 @@ class textTableBus(object):
 
     def write_byte_data(self, address, register, value):
         """"Sets/Clears a pixel into data buffer array"""
-        self._logger.debug('write_byte_data({0},{1},{2})' .format(address, register, value))
-        # Odd/even = colour for the bi-colour display
+        # self._logger.debug('write_byte_data({0},{1},{2})' .format(address, register, value))
+        # Odd/even register = colour for the bi-colour display
         [row, mod] = divmod(register, 2)
-        # colourBit is the pixel colour we're turning on or off in the colour bit mask
+        # colourBit is the pixel colour we're turning on or off in the colour bit map
         colourBit = 0 if mod else 1
-        self._logger.debug('row:{0} colour:{1} ' .format(row, colourBit))
+        # self._logger.debug('row:{0} colour:{1} ' .format(row, colourBit))
 
-        # Map to array row from bitfield
+        # Map to array row from column-pixel bitfield in value
         for j in range(8):
             # Set this colour's bit in our colour mask
             self.data[row][j] = self.set_bit(self.data[row][j], colourBit, (value & pow(2, j)))
-            self._logger.debug('pixel j:{0} set:{2} now:{1:02b} '.format(j, self.data[row][j], (value & pow(2, j))))
+            # self._logger.debug('pixel j:{0} set:{2} now:{1:02b} '.format(j, self.data[row][j], (value & pow(2, j))))
 
         # print("Draw: row:{0} colour:{1} cells:{2:08b}(0x{2:x})".format(row, colourBit, value))
+        # if register == 0x0f: # this optimises the drawing speed, if we know we always render the whole matrix! 0xF is the last row.
         self.draw()
 
     def render_pixel(self, value):
-        """Decodes the GR=Y combination"""
-        return {0: ' ', 1: 'G', 2: 'R', 3: 'Y'}[value]
+        """Decodes the G+R=Y combination to human readable. They could be console colour codes!"""
+        return {0b00: ' ', 0b01: 'G', 0b10: 'R', 0b11: 'Y'}[value]
+        # colours, but the codes stretch the table, need to set with of 15 but it's spaced out and flickers a bit.
+        # return {0b00: ' ', 0b01: "\33[42m \33[0m", 0b10: '\33[41m \33[0m', 0b11: '\33[43m \33[0m'}[value]
+        # return {0b00: ' ', 0b01: '|', 0b10: '-', 0b11: '+'}[value]
 
+    # this could be in a general library: from http://stackoverflow.com/a/12174051/209288
     def set_bit(self, v, index, x):
         """Set the index:th bit of v to 1 if x is truthy, else to 0, and return the new value."""
         mask = 1 << index  # Compute mask, an integer with just bit 'index' set.
