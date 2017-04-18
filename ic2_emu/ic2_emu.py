89656035f0b190cb23f67fd1b4c2d5f1d51202f2
Pip Jones <code@scipilot.org>
2017-04-18 17:49:29 +1000

    Working draft of the TextTable emulator

--- ic2_emu/ic2_emu.py
+++ ic2_emu/ic2_emu.py
@@ -6,6 +6,11 @@
 # Based on the INTERFACE of Adafruit_GPIO/I2C.py created by Tony DiCola which is Copyright (c) 2014 Adafruit Industries
 
 import logging
+import math
+import array
+
+# todo: make a DI interface for the renderer?
+from texttable import Texttable
 
 def get_i2c_device(address, busnum=None, i2c_interface=None, **kwargs):
     """Return an I2C device for the specified address and on the specified bus.
@@ -28,8 +33,9 @@ class Device(object):
         self._address = address
         if i2c_interface is None:
             # Use pure python I2C interface if none is specified.
-            #import .asciiBus
-            self._bus = asciiBus(busnum)
+            #import asciiBus
+            # self._bus = asciiLogBus(busnum)
+            self._bus = textTableBus(busnum)
         else:
             # Otherwise use the provided class to create an smbus interface.
             self._bus = i2c_interface(busnum)
@@ -47,17 +53,81 @@ class Device(object):
         self._logger.debug(".write8:Writing value:0x%02X to register 0x%02X", value, register)
         self._bus.write_byte_data(self._address, register, value)
 
-class asciiBus(object):
+
+"""
+The rows/columns are represented by register/value combinations.
+- R00 = Green pixels in row 1
+- R01 = Red pixels in row 1
+- R0E = Green pixels in row 8
+- R0F = Red pixels in row 8
+- Red+Green = Yellow 
+
+The columns are set/cleared by bits in the data: 
+- 00 = all off, 
+- 01, 02, 04 ... 80 = individual pixels
+- FF = all on
+
+Address = 112
+
+The emulator has to update the output display per-pixel change (there is no "frame").
+"""
+
+class asciiLogBus(object):
     """
     Writes all output into ASCII
     """
 
     def __init__(self, busnum):
-        self._logger = logging.getLogger('I2C_Emu.AsciiBus')
+        self._logger = logging.getLogger('I2C_Emu.asciiLogBus')
 
     def write_i2c_block_data(self, address, register, data):
         self._logger.debug('write_i2c_block_data({0},{1},{2})' .format(address, register, data))
 
     def write_byte_data(self, address, register, value):
         self._logger.debug('write_byte_data({0},{1},{2})' .format(address, register, value))
+
+class textTableBus(object):
+    """
+    Writes all output into ASCII Table on the console
+    Using https://github.com/foutaise/texttable/
+    """
+
+    def __init__(self, busnum):
+        self._logger = logging.getLogger('I2C_Emu.textTableBus')
+        # initialise internal data buffer array
+        self.data = [[0 for j in range(8)] for i in range(8)]
+
+    def draw(self):
+        table = Texttable()
+        #table.set_cols_align(["l", "r", "c"])
+        #table.set_cols_valign(["t", "m", "b"])
+        # Column indexes
+        table.add_row(["1", "2", "3", "4", "5", "6", "7", "8"])
+        for row in self.data:
+            # todo prepend row index?
+            table.add_row([col for col in row])
+        print(table.draw() + "\n")
+
+    def write_i2c_block_data(self, address, register, data):
+        self._logger.debug('write_i2c_block_data({0},{1},{2})' .format(address, register, data))
+
+    def write_byte_data(self, address, register, value):
+        self._logger.debug('write_byte_data({0},{1},{2})' .format(address, register, value))
+        # Set pixel into data buffer array
+        # Odd/even = colour
+        [row, mod] = divmod(register, 2)
+        # TODO need to check neighbour to make Y, or use 16 rows to show "RG"? or just separate them all out into 3 virtual pixels?
+        colour = ""
+        if(register % 2 == 0):
+            colour = "G"
+        else:
+            colour = "R"
+        self._logger.debug('row:{0} colour:{1} ' .format(row, colour))
+        # Map to array
+        for j in range(8):
+            # Check if each bit is set
+            self._logger.debug('pixel j:{0} set:{1} '.format(j, value & pow(2,j)))
+            self.data[row][j] = colour if (value & pow(2,j)) else ""
+
+        self.draw()
 
